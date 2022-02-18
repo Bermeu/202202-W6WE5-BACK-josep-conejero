c248f60509c69be735fe363247655cf89e1e16ed
Josep Conejero <bermeu@gmail.com>
2022-02-18 22:07:38 +0100

    Added connection to db

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const debug = require("debug")("robots:root");
-// const connectToDataBase = require("./db");
+const connectToDataBase = require("./src/db/index");
 const risetheServer = require("./src/server/index");
 
 const port = process.env.SERVER_PORT || 4000;
-// const mongoConnection = process.env.MONGO_STRING;
+const mongoConnection = process.env.MONGO_STRING;
 
 (async () => {
   try {
-    // await connectToDataBase(mongoConnection);
+    await connectToDataBase(mongoConnection);
     await risetheServer(port);
   } catch (error) {
     debug(`Error: ${error.message}`);
